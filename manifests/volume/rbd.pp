da5365d9711ac412d6983b1469d56d1e719b2955
Michael Jeanson <mjeanson@gmail.com>
2013-06-19 15:45:04 -0400

    Add upstart override to specify rbd_user

--- manifests/volume/rbd.pp
+++ manifests/volume/rbd.pp
@@ -7,8 +7,14 @@ class cinder::volume::rbd (
 
   include cinder::params
 
+  file {"/etc/init/cinder-volume.override":
+    content => "env CEPH_ARGS=\"--id ${rbd_user}\"",
+    require => Package['cinder'],
+    notify  => Service['cinder-volume']
+  }
+
   cinder_config {
-    'DEFAULT/volume_driver':    value => 'cinder.volume.driver.RBDDriver';
+    'DEFAULT/volume_driver':    value => 'cinder.volume.drivers.rbd.RBDDriver';
     'DEFAULT/rbd_pool':         value => $rbd_pool;
     'DEFAULT/rbd_user':         value => $rbd_user;
     'DEFAULT/rbd_secret_uuid':  value => $rbd_secret_uuid;
